4d9ba05c31cd47c6b86b995c63c5a6023279dd2b
mwylegly <michael.wylegly@gmail.com>
2012-05-16 13:00:41 -0600

    this is a commit

--- mwylegly_0503_01/C4WorkSpace.m
+++ mwylegly_0503_01/C4WorkSpace.m
@@ -11,7 +11,7 @@
 @implementation C4WorkSpace{
     
     int pointCount;
-    CGPoint pointList[7];
+    CGPoint pointList[25];
     C4Shape *elPoints;
     float xPointValue, yPointValue;
     int pointOriginWidth, pointOriginHeight;
@@ -21,13 +21,15 @@
 }
 
 -(void)setup {
-    pointCount = 7;
+    pointCount = 25;
     
     for (int i = 0; i < pointCount; i++) {
         
-        pointOriginWidth = [C4Math randomIntBetweenA:(i*2 + [C4Math randomInt:100]) andB:(self.canvas.frame.size.width - (i*2 + [C4Math randomInt:100]))];
+        pointOriginWidth = [C4Math randomIntBetweenA:(i*2 + [C4Math randomInt:100]) 
+                                                andB:(self.canvas.frame.size.width - (i*2 + [C4Math randomInt:100]))];
         
-        pointOriginHeight = [C4Math randomIntBetweenA:(i + [C4Math randomInt:100]) andB:(self.canvas.frame.size.width - (i*2 + [C4Math randomInt:100]))];
+        pointOriginHeight = [C4Math randomIntBetweenA:(i*2 + [C4Math randomInt:100]) 
+                                                 andB:(self.canvas.frame.size.height - (i*2 + [C4Math randomInt:100]))];
         
         pointList[i] = CGPointMake(pointOriginWidth, pointOriginHeight); 
         //C4Log(@"%4.2f,%4.2f", pointList[i]);
@@ -46,23 +48,39 @@
     //C4Log(@"%4.2f,%4.2f", pointList[]);
     
     for (int index = 0; index < pointCount; index++) {
-        for (int index2 = 0; index2 < pointCount; index2 ++) {
+        for (int index2 = index+1; index2 < pointCount; index2 ++) {
+            
+            
             
             CGPoint linePoints[2] = {
                 CGPointMake(pointList[index].x, pointList[index].y),
                 CGPointMake(pointList[index2].x, pointList[index2].y)
             };
             
+            CGFloat deltay = linePoints[0].y - linePoints[1].y;
+            CGFloat deltax = linePoints[0].x - linePoints[1].x;
+            CGFloat distance = sqrt(pow(deltax,2) + pow(deltay,2));
+            
             C4Shape *webbing = [C4Shape line:linePoints];
-            webbing.lineWidth = 2.5f;
-            webbing.strokeColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:.05];
+            webbing.animationDuration = 0.0f;
+            CGFloat alpha = 0.0f;
+            
+            if (distance > 500) {
+                alpha = 0.1;
+                webbing.lineWidth = 1.5f;
+            }  else if(distance > 200 && distance < 500) {
+                alpha = 0.33;
+                webbing.lineWidth = 2.0f;
+            } else {
+                alpha = .75;
+                webbing.lineWidth = 3.0f;
+            }
+            
+            webbing.strokeColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:alpha];
             [self.canvas addShape:webbing];
         }
     }
-    
-    //C4Log(@"%4.2f,%4.2f", xPointValue, yPointValue);
-
-    
+        
 }
 
 
